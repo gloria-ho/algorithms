b90eafdb43b3c00205c948d5e87a3ccc48c3e95d
Gloria H <silverdragonia@gmail.com>
2018-08-01 13:56:41 -0400

    initial commit

---
+++ convert_num_to_reversed_array_of_digits.py
@@ -0,0 +1,15 @@
+# https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+
+# Given a random number:
+
+# C#: long;
+# C++: unsigned long;
+# You have to return the digits of this number within an array in reverse order.
+
+# Example:
+
+# 348597 => [7,9,5,8,4,3]
+
+def digitize(n):
+  lst = [int(i) for i in str(n)]
+  return lst[::-1]
