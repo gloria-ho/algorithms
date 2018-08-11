2a2871ae59e3388de1aad2bd81df2a11cf95b26f
Gloria H <silverdragonia@gmail.com>
2018-08-11 18:22:28 -0400

    initial commit

---
+++ do_i_get_a_bonus.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/
+
+# It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+# Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+# If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+# Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS and Java) or "$" (C#, C++, Ruby, Clojure, Elixir, PHP and Python).
+
+def bonus_time(salary, bonus):
+  return '$'+str(salary*10) if bonus == True else '$'+str(salary)
