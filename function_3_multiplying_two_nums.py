203ea3c73f9e5bbbc6b9bebb10bccc3b777f063b
Gloria H <silverdragonia@gmail.com>
2018-08-12 13:23:18 -0400

    initial commit

---
+++ function_3_multiplying_two_nums.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+# This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+
+def multiply(x,y):
+  return x*y
