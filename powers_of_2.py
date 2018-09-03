246b8e92661885a018f48d348c9108cef738c091
Gloria H <silverdragonia@gmail.com>
2018-09-03 09:05:16 -0700

    initial commit

---
+++ powers_of_2.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/57a083a57cb1f31db7000028
+
+# Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n (inclusive).
+
+# Examples
+# n = 0  ==> [1]        # [2^0]
+# n = 1  ==> [1, 2]     # [2^0, 2^1]
+# n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+def powers_of_two(n):
+  x = 0
+  result = []
+  while x <= n:
+    result.append(2**x)
+    x += 1
+  return result
