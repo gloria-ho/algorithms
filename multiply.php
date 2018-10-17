85d90a4f259ff613240ff5436baf59aee70a5259
Gloria H <silverdragonia@gmail.com>
2018-10-16 22:44:59 -0400

    initial commit

---
+++ multiply.php
@@ -0,0 +1,10 @@
+<?php
+  // https://www.codewars.com/kata/multiply
+
+  // The code does not execute properly. Try to figure out why.
+
+  function multiply($a, $b) {
+    return $a * $b;
+  }
+
+?>
