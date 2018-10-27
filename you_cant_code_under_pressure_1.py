06f017ebbaeafb1c8f0b4e5f894805f24e9d24f6
Gloria H <silverdragonia@gmail.com>
2018-10-26 22:42:05 -0400

    initial commit

---
+++ you_cant_code_under_pressure_1.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/you-cant-code-under-pressure-number-1/python
+
+# Code as fast as you can! You need to double the integer and return it.
+
+def doubleInteger(i):
+  return i * 2
