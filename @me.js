006fd53b6e59a1dbe8175b5044d064c4ec53bce1
Gloria H <silverdragonia@gmail.com>
2018-12-18 09:21:40 -0500

    updating

--- @me.js
+++ @me.js
@@ -1,15 +1,18 @@
+//@Me
+
 // Given an array of twitter handles, print a string that @s all the users.
 
 // ex: ['Vestwell', 'GirlDevelopIt'] -> '@Vestwell @GirlDevelopIt'
 
 
-// using a for loop, add the @symbol before each username in the array
+// using a for loop, create a new array withthe @symbol before each username in the original array
 // return the array joined by spaces
 function atUsers(users) {
+  atUsers = users;
   for (i in users) {
-    users[i] = '@' + users[i];
+    atUsers[i] = '@' + users[i];
   }
-  return users.join(' ');
+  return atUsers.join(' ');
 }
 
-console.log(atUsers(['Vestwell', 'GirlDevelopIt'])) // '@Vestwell @GirlDevelopIt'
+console.log(atUsers(['Vestwell', 'GirlDevelopIt'])); // '@Vestwell @GirlDevelopIt'
