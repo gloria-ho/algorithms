443b52b37e8dfe1e4cb3a387711db0ebb15fcce0
Gloria H <silverdragonia@gmail.com>
2018-12-26 20:03:00 -0500

    initial commit

---
+++ removeDuplicate.js
@@ -0,0 +1,16 @@
+// Remove Duplicates
+// Write a function that removes duplicates from an array of numbers.
+
+//  Ex: [2, 4, 7, 22, 3, 4, 2, 7] -> [2, 3, 4, 7, 22]
+
+const removeDuplicates = (arr) => {
+  result = []
+  for (let i = 0; i < arr.length; i ++ ) {
+    if (result.indexOf(arr[i]) === -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(removeDuplicates([2, 4, 7, 22, 3, 4, 2, 7])) // [2, 3, 4, 7, 22
