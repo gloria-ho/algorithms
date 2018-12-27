b1d4680fa99ec5059ef125409ced7b0621e04d3a
Gloria H <silverdragonia@gmail.com>
2018-12-27 17:10:03 -0500

    initial commit

---
+++ characaterRepeats.js
@@ -0,0 +1,12 @@
+// Character Repeats
+// Write a function that takes in two parameters: a paragraph of text and a letter, and returns the number of times that letter appears in the paragraph.
+
+const characterRepeats = (paragraph, letter) => {
+  let target = new RegExp(letter,'g');
+  return (paragraph.match(target) || []).length;
+}
+
+const myPara = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt dignissimos natus praesentium, cum dicta alias sunt voluptates consequatur aliquam porro impedit. Provident delectus iste distinctio accusantium maiores at a magni!';
+
+console.log(characterRepeats(myPara, 'a'));
+
