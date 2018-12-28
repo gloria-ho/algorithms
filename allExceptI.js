7480a3b1e0dac315f621bf3b67b415f9b9aa0544
Gloria H <silverdragonia@gmail.com>
2018-12-28 09:46:26 -0500

    initial commit

---
+++ allExceptI.js
@@ -0,0 +1,20 @@
+// All Except i
+// Given an array of integers, create a new array with the product of all the integers except for itself.
+
+//  Ex: [1, 2, 3] => [6, 3, 2] 
+
+const allExceptI = (arr) => {
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    let product = 1;
+    for (let j = 0; j <arr.length; j++) {
+      if (j != i) {
+        product *= arr[j];
+      }
+    }
+    result.push(product);
+  }
+  return result;
+}
+
+console.log(allExceptI([1, 2, 3])) // [6, 3, 2] 
